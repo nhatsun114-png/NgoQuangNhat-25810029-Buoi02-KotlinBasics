61c45bf87352487a1954192b2a9f70cbbf19c484
tanhvnc <155526178+tanhvnc@users.noreply.github.com>
2026-09-25 15:57:19 +0700

    Hoàn thành bài 2 giới thiệu bản thân

---
+++ app/src/main/java/edu/hcmute/helloworld/Bai02_GioiThieuBanThan.kt
@@ -0,0 +1,9 @@
+fun main() {
+
+    val hoTen: String = "Ngô Quang Nhật"
+    val namSinh: Int = 2004
+    val namHienTai: Int = 2026
+    val tuoi: Int = namHienTai - namSinh
+
+    println("Xin chào thầy, em tên là $hoTen. Năm nay tôi $tuoi tuổi.")
+}
