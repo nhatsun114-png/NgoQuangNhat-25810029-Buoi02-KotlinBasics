d4a0b9d30839f798cc18428ba775e215cd969319
tanhvnc <155526178+tanhvnc@users.noreply.github.com>
2026-09-25 15:10:29 +0700

    Bài 1

---
+++ app/src/main/java/edu/hcmute/helloworld/MainActivity.kt
@@ -0,0 +1,30 @@
+package edu.hcmute.helloworld
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        printHelloWorld()
+    }
+
+    private fun printHelloWorld() {
+        println("Hello World!")
+        Log.d("MainActivity", "Hello World!")
+        Toast.makeText(this, "Hello World!", Toast.LENGTH_SHORT).show()
+    }
+}
